3aa55d29b012b1c561c4bceb85bf8f2ada27713f
Ксения Кострова <kseniya.bolshakova@infourok.biz>
2025-12-19 17:05:44 +0300

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/epub-reader/',
   plugins: [react()],
   assetsInclude: ['**/*.epub'],
 })
